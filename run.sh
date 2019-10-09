d4dd1b2c4e51d2109b6a42eab15728a100c2dd66
dophist <jerry.ejwt@qq.com>
2019-10-09 13:20:59 +0800

    minor config update

--- run.sh
+++ run.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    echo "run.sh url.list dir"
+    echo "run.sh <url.list> <dir>"
+    echo "  e.g.: run.sh urls.txt test_201909"
     exit 1;
 fi
 
-youtube_dl=/home/dophist/work/git/ASR_TIOBE/bin/youtube-dl
+youtube_dl=~/work/TIOBE/bin/youtube-dl
 
 url_list=$1
 dir=$2
 
 rm -rf $dir && mkdir -p $dir/{raw,wav}
-cp $url_list config.txt $dir/
+
+cp $url_list $dir/url.list
+cp config.txt $dir/config.txt
+
 
 cd $dir
 $youtube_dl \
     --config-location config.txt \
     --batch-file url.list \
-    --output "raw/%(title)s.%(ext)s" \
+    --output "raw/%(id)s__%(title)s.%(ext)s" \
     --exec 'ffmpeg -i {} -ac 1 -ar 16000 wav/`basename {}`'
-
 cd -
+
