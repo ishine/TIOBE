815597f387f7fee82c3bd8635da284db2ac8e73f
dophist <jerry.ejwt@qq.com>
2020-03-23 17:52:07 +0800

    add top level script for testing matrix setup

--- test_env/aliyun/run.sh
+++ test_env/aliyun/run.sh
@@ -1,2 +1,11 @@
+if [ $# -ne 3 ]; then
+  echo "run.sh <max_num_utts> <test_set_path> <result_dir_name>"
+  exit 1;
+fi
+
+max_num_utts=$1
+test_set_path=$2
+result_dir_name=$3
+
 python3 update_token.py
-sh ../utils/test.sh 10000 ../../dataset/AISHELL-2/iOS/test RES_AISHELL-2-IOS_20191010
+sh ../utils/test.sh $max_num_utts $test_set_path $result_dir_name
