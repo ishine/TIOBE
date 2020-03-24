c1d1c5ef0cd038a51f17d3086daa3bbad5fb8399
dophist <jerry.ejwt@qq.com>
2020-03-24 15:45:04 +0800

    log into working dir

--- test_env/run.sh
+++ test_env/run.sh
@@ -1,6 +1,6 @@
 service_list="aliyun baidu google iflytek tencent yitu"
 test_set_list="cctv_news"
-date=20200323
+date=20200324
 max_num_utts=10000
 RESULT_DIR=`readlink -f RESULT_DATA`
 DATASET_DIR=`readlink -f ../dataset`
@@ -11,7 +11,10 @@ for service in $service_list; do
     for test_set in $test_set_list; do
         echo "==> Testing ASR:$service TEST_SET:$test_set DATE:$date NUM_UTTS:$max_num_utts"
         dir_name=${service}__${test_set}__${date}__${max_num_utts}
-        sh run.sh $max_num_utts $DATASET_DIR/$test_set $RESULT_DIR/$dir_name
+        mkdir -p $RESULT_DIR/$dir_name
+        nohup sh run.sh $max_num_utts $DATASET_DIR/$test_set $RESULT_DIR/$dir_name >& $RESULT_DIR/$dir_name/log.run &
     done
     cd -
 done
+wait
+echo "Done"
