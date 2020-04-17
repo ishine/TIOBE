607ca63926edf87c88849b444e2df9d8870dc7af
dophist <jerry.ejwt@qq.com>
2020-04-18 05:12:09 +0800

    test env dir restructured

--- test_env/run.sh
+++ test_env/run.sh
@@ -1,20 +1,24 @@
-service_list="aliyun baidu google iflytek tencent yitu"
-test_set_list="cctv_news"
-date=20200324
-max_num_utts=10000
-RESULT_DIR=`readlink -f RESULT_DATA`
-DATASET_DIR=`readlink -f ../dataset`
+#service_list="aliyun baidu google iflytek sogou tencent yitu microsoft"
+service_list="iflytek"
+#testset_list="AISHELL-2_iOS_test cctv_news"
+testset_list="cctv_news"
+max_num_utts=100
 
-for service in $service_list; do
-    cd $service
-    pwd
-    for test_set in $test_set_list; do
-        echo "==> Testing ASR:$service TEST_SET:$test_set DATE:$date NUM_UTTS:$max_num_utts"
-        dir_name=${service}__${test_set}__${date}__${max_num_utts}
-        mkdir -p $RESULT_DIR/$dir_name
-        nohup sh run.sh $max_num_utts $DATASET_DIR/$test_set $RESULT_DIR/$dir_name >& $RESULT_DIR/$dir_name/log.run &
+#----------------------------
+date=`date +%Y%m%d`
+RESULT=`readlink -f result`
+DATASET=`readlink -f dataset`
+
+for x in $service_list; do
+    cd service/$x
+    for y in $testset_list; do
+        echo "==>Testing Service:$x TEST_SET:$y DATE:$date NUM_UTTS:$max_num_utts"
+        job=${date}__${x}__${y}__${max_num_utts}
+        mkdir -p $RESULT/$job
+        nohup sh run.sh $max_num_utts $DATASET/$y $RESULT/$job >& $RESULT/$job/log.run &
     done
     cd -
 done
 wait
 echo "Done"
+
